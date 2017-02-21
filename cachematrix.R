5b20e4810caec3a6880774c0124e3e37f6c2cc71
sabrina3399 <zhengyixue@gmail.com>
2017-02-20 21:35:57 -0500

    Cache Matrix.R
    ## write an R function is able to cache potentially time-consuming computations. 
    
    
    ## Save original data
    
    makeCacheMatrix <- function(x = matrix()) {
            m <- matrix()
            set <- function(y){
                   x <<-y
                    m<<-matrix()}
            get <- function() x
            setinverse <- function (Inverse) m <<-Inverse
            getinverse <-funtion() m
            list(set=set, get=get,getinverse=getinverse,setinverse=setinverse)
            
    }
    
    
    ## Cache inversematrix
    
    cacheSolve <- function(x, ...) {
            m <- x$getinverse()
            if(!is.null(m)){
                    message("getting cached data")
                    return(m)
                    }
            data <- x$get()
            m <- Solve(data,...)
            x$setinverse(m)
            m
            ## Return a matrix that is the inverse of 'x'
    }

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## write an R function is able to cache potentially time-consuming computations. 
 
-## Write a short comment describing this function
+
+## Save original data
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- matrix()
+        set <- function(y){
+               x <<-y
+                m<<-matrix()}
+        get <- function() x
+        setinverse <- function (Inverse) m <<-Inverse
+        getinverse <-funtion() m
+        list(set=set, get=get,getinverse=getinverse,setinverse=setinverse)
+        
 }
 
 
-## Write a short comment describing this function
+## Cache inversematrix
 
 cacheSolve <- function(x, ...) {
+        m <- x$getinverse()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+                }
+        data <- x$get()
+        m <- Solve(data,...)
+        x$setinverse(m)
+        m
         ## Return a matrix that is the inverse of 'x'
 }
